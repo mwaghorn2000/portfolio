1e7df2e11ebc7aa748a5c69643f5b1ebf4991fc9
Mitchell Waghorn <mitchellwaghorn@Mitchells-MacBook-Pro.local>
2024-01-29 15:50:33 +1100

    added other projects to portfolio

--- app/Projects/Projects.tsx
+++ app/Projects/Projects.tsx
@@ -7,26 +7,34 @@ const Projects: React.FC = () => {
             <div className="about-me py-[20px] mx-auto subpixel-antialiased lg:py-[100px] w-full lg:w-full">
                 <h1 className="py-3 text-2xl font-extrabold text-center">&lt;Projects /&gt;</h1>
                 <h2 className="text-center pb-[35px]">Click on the images to go to the project</h2>
-                <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 justify-evenly'>
-                    <div className='m-auto text-center'>
-                        <a href="https://mwaghorn2000.github.io/calculator/" target="_blank"><Image className="m-auto" src="/../../../calculator.png" alt="sampleImage" width={360} height={360} /></a>
+                <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4 justify-around'>
+                    <div className='m-auto text-center h-[550px]'>
+                        <a href="https://mwaghorn2000.github.io/calculator/" target="_blank">
+                            <Image className="m-auto h-[363px]" src="/../../../calculator.png" alt="Image of calculator project" width={360} height={360} />
+                        </a>
                         <h2 className="text-xl py-3">Calculator</h2>
-                        <p>First project i made which used CSS, HTML, and JS. Was proud when i made this, it can even register keyboard inputs</p>
+                        <p className='h-[120px]'>First project i made which used CSS, HTML, and JS. Was proud when i made this, it can even register keyboard inputs</p>
                     </div>
-                    <div className='m-auto text-center'>
-                        <a href="https://mwaghorn2000.github.io/calculator/" target="_blank"><Image className="m-auto" src="/../../../calculator.png" alt="sampleImage" width={360} height={360} /></a>
-                        <h2 className="text-xl py-3">Calculator</h2>
-                        <p>First project i made which used CSS, HTML, and JS. Was proud when i made this, it can even register keyboard inputs</p>
+                    <div className='m-auto text-center h-[550px]'>
+                        <a href="https://github.com/mwaghorn2000/RISKIT" target="_blank" >
+                            <Image className="m-auto h-[363px]" src="/../../../riskit_logo.png" alt="image of riskit logo" width={360} height={360} />
+                        </a>
+                        <h2 className="text-xl py-3">RISKIT</h2>
+                        <p className='h-[120px]'>
+                            This is a online casino game im working on with two other students at UNSW. 
+                            <a className="text-lime-600 hover:text-lime-900 transition-colors duration-200" href="https://github.com/Yangstaboi" target='_blank'> William Yang </a>
+                             and 
+                            <a className="text-lime-600 hover:text-lime-900 transition-colors duration-200" href="https://github.com/Arnold45202" target="_blank"> Jackson Wang</a>.
+                            This is a full stack project, As of right now it's still a WIP, and the only game left is poker, which is the most complex part of the app.
+                            It uses react, typescript, socket.io and firebase under the hood.
+                        </p>
                     </div>
-                    <div className='m-auto text-center'>
-                        <a href="https://mwaghorn2000.github.io/calculator/" target="_blank"><Image className="m-auto" src="/../../../calculator.png" alt="sampleImage" width={360} height={360} /></a>
-                        <h2 className="text-xl py-3">Calculator</h2>
-                        <p>First project i made which used CSS, HTML, and JS. Was proud when i made this, it can even register keyboard inputs</p>
-                    </div>
-                    <div className='m-auto text-center'>
-                        <a href="https://mwaghorn2000.github.io/calculator/" target="_blank"><Image className="m-auto" src="/../../../calculator.png" alt="sampleImage" width={360} height={360} /></a>
-                        <h2 className="text-xl py-3">Calculator</h2>
-                        <p>First project i made which used CSS, HTML, and JS. Was proud when i made this, it can even register keyboard inputs</p>
+                    <div className='m-auto text-center h-[550px]'>
+                        <a href="https://github.com/mwaghorn2000/portfolio" target="_blank">
+                            <Image className="m-auto h-[363px]" src="/../../../portfolio_img.png" alt="image of homepage of portfolio" width={360} height={360} />
+                        </a>
+                        <h2 className="text-xl py-3">My Portfolio</h2>
+                        <p>The website you are on right now. Using next.js for routing, and tailwindCSS for styling. Also features a blog which I use to discuss my projects, and</p>
                     </div>
                 </div>
             </div>
--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <div className="m-auto w-1/2 text-center absolute top-1/2 inset-x-0 -translate-y-2/3">
-        <h1 className="text-6xl font-bold">&lt; Mitchell Waghorn /&gt;</h1>
-        <h2 className="py-3 text-4xl mb-3.5">
+      <div className="m-auto w-2/3 text-center absolute top-1/2 inset-x-0 -translate-y-2/3">
+        <h1 className=" md:text-4xl lg:text-6xl sm:text-3xl text-2xl font-bold">&lt; Mitchell Waghorn /&gt;</h1>
+        <h2 className="py-3 lg:text-4xl md:text-3x sm:text-2xl text-1xl mb-3.5">
           Welcome to my web portfolio<span className="typing-cursor ml-2">|</span>
         </h2>
         <Link className="py-3.5 px-[50px] bg-lime-400 text-center rounded-lg transition-colors hover:bg-lime-500 duration-300" href="/AboutMe">Enter </Link>
