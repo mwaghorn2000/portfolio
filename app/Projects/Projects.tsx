9ae548f8fa5009cba8c3d9f9e2eb57826f696091
Mitchell Waghorn <m.waghorn2000@gmail.com>
2024-01-30 15:32:54 +1100

    modified riskits description

--- app/Projects/Projects.tsx
+++ app/Projects/Projects.tsx
@@ -8,7 +8,7 @@ const Projects: React.FC = () => {
                 <h1 className="py-3 text-2xl font-extrabold text-center">&lt;Projects /&gt;</h1>
                 <h2 className="text-center pb-[35px]">Click on the images to go to the project</h2>
                 <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 justify-around mb-[20px] mx-[20px]'>
-                    <div className='m-auto text-center h-[550px]'>
+                    <div className='m-auto text-center sm:max-h-[700px] sm:min-h-auto h-[550px]'>
                         <a href="https://mwaghorn2000.github.io/calculator/" target="_blank">
                             <Image className="m-auto h-[363px]" src="/calculator.png" alt="Image of calculator project" width={360} height={360} />
                         </a>
@@ -22,13 +22,13 @@ const Projects: React.FC = () => {
                             </div>
                         </a>
                         <h2 className="text-xl py-3">RISKIT</h2>
-                        <p className='h-[120px]'>
-                            This is a online casino game im working on with two other students at UNSW.
-                            <a className="text-lime-600 hover:text-lime-900 transition-colors duration-200" href="https://github.com/Yangstaboi" target='_blank'> William Yang </a>
-                            and
-                            <a className="text-lime-600 hover:text-lime-900 transition-colors duration-200" href="https://github.com/Arnold45202" target="_blank"> Jackson Wang</a>.
-                            This is a full stack project, As of right now it&apos;s still a WIP, and the only game left is poker, which is the most complex part of the app.
-                            It uses react, typescript, socket.io and firebase under the hood.
+                        <p>
+                            RISKIT, an online casino game developed by UNSW students Myself, 
+                            <a className="text-lime-600 hover:text-lime-900 transition-colors duration-200" href="https://github.com/Yangstaboi" target='_blank'> William Yang</a>, 
+                            and <a className="text-lime-600 hover:text-lime-900 transition-colors duration-200" href="https://github.com/Arnold45202" target="_blank"> Jackson Wang</a>. 
+                            Our app, currently a work in progress, features a variety of casino games, with 
+                            Poker as our upcoming addition. Built using React, TypeScript, Socket.IO, and Firebase, "Riskit" offers 
+                            an engaging and dynamic gaming experience. This will be getting deployed once Poker is complete
                         </p>
                     </div>
                     <div className='m-auto text-center h-[550px]'>
